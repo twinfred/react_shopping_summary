2f4531dea37e49609b612f3e811412ceb787c22e
Tim Winfred <twinfred@gmail.com>
2019-05-08 07:31:13 -0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Container } from 'react-bootstrap';
+
+import SubTotal from './components/Subtotal/Subtotal';
+import PickupSavings from './components/PickupSavings/PickupSavings';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends Component {
+  constructor(props) {
+    super(props);
 
-export default App;
+    this.state = {
+      total: 100,
+      pickupSavings: 3.85
+    }
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <Container className="purchase-card">
+          <SubTotal price={this.state.total.toFixed(2)} />
+          <PickupSavings price={this.state.pickupSavings} />
+        </Container>
+      </div>
+    );
+  }
+}
